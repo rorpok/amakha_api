a826b9a9efc744c0918e26f485a8aab86371bfaf
Allan Dantas <dantas@mileva.io>
2020-03-23 19:40:53 -0500

    Feature: Define atributos da tabela 'box' na respectiva migration.

--- database/migrations/2020_03_23_173833_create_box_table.php
+++ database/migrations/2020_03_23_173833_create_box_table.php
@@ -14,9 +14,11 @@ class CreateBoxTable extends Migration
     public function up()
     {
         Schema::create('box', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+			
+            $table->id( 'idBox' );
+			$table->string( 'name', 120 );
+        
+		});
     }
 
     /**
